9df4fa46be3b6f4dad78ec9497c7ce0a7fa75877
nazmul.hossain.nihal <nazmul.hossain.nihal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-22 16:51:02 +0000

    UpDate to 1.2
    git-svn-id: https://plugins.svn.wordpress.org/wpcss/tags/1.2@1096562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display/settings.php
@@ -0,0 +1,10 @@
+<?php 
+	/******************************
+	-Display files are included here
+	******************************/
+	include("inc/site.php"); // Css For Site
+	
+	include("inc/login.php"); // Css For Login Screen
+	
+	include("inc/admin.php"); // Css For Admin Dashboard
+?>
